23f34fee49cdd896a0bb1a51512ab345de11bf57
Arturo Escudero <darkturo@gmail.com>
2025-06-07 15:56:26 +0200

    Add support to echo

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,11 @@
 #[allow(unused_imports)]
+
+use std::str;
 use std::io::{self, Write};
+
+fn echo(msg: &[String]) {
+    println!("{}", msg.join(" "));
+}
 
 fn main() {
 
@@ -11,11 +17,21 @@ fn main() {
         let mut input = String::new();
         io::stdin().read_line(&mut input).unwrap();
     
-        let command = input.trim();
-        if command == "exit 0" {
-            break;
+        let mut parts = input.split_whitespace();
+        let command = parts.next();
+        let args : Vec<String> = parts.map(str::to_string).collect();
+        match command {
+            Some("exit") => {
+                if args.is_empty() || args == ["0"] {
+                    break;
+                } else {
+                    println!("Wrong exit");
+                }
+            },
+            Some("echo") => echo(&args),
+            Some(_) => println!("{}: command not found", input.trim()),
+            None => continue,
         }
-        print!("{}: command not found\n", command);    
     }
 }
 
