b44bd1110f222878bbda6f76546a2bfdcee85b0e
Arturo Escudero <darkturo@gmail.com>
2025-06-07 16:18:37 +0200

    Add a way to get basic info about the builtins

--- src/main.rs
+++ src/main.rs
@@ -2,13 +2,30 @@
 
 use std::str;
 use std::io::{self, Write};
+use std::collections::HashMap;
 
-fn echo(msg: &[String]) {
+fn echo_cmd(msg: &[String]) {
     println!("{}", msg.join(" "));
 }
 
-fn main() {
+fn type_cmd(args: &[String]) {
+    let mut builtins = HashMap::new();
+    builtins.insert("echo", "echo <message>");
+    builtins.insert("exit", "exit <exit_status>");
+    builtins.insert("type", "type <command>");
 
+    let mut command = args[0].clone();
+    if args.is_empty() || args.len() > 1 {
+        command = "type".to_string();
+    }
+
+    match builtins.get(command.as_str()) {
+        Some(&help) => println!("{}", help),
+        _ => println!("Error: invalid argments"),
+    }
+}
+
+fn main() {
     loop {
         print!("$ ");
         io::stdout().flush().unwrap();
@@ -28,7 +45,8 @@ fn main() {
                     println!("Wrong exit");
                 }
             },
-            Some("echo") => echo(&args),
+            Some("echo") => echo_cmd(&args),
+            Some("type") => type_cmd(&args),
             Some(_) => println!("{}: command not found", input.trim()),
             None => continue,
         }
