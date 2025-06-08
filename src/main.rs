b930bfaaaae9eb5ff4a357ac1896f2a189459787
Arturo Escudero <darkturo@gmail.com>
2025-06-08 16:55:55 +0200

    The type builtin: executable files

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,10 @@
 #[allow(unused_imports)]
 
+use std::env;
 use std::str;
 use std::io::{self, Write};
 use std::collections::HashMap;
+use walkdir::WalkDir;
 
 fn echo_cmd(msg: &[String]) {
     println!("{}", msg.join(" "));
@@ -31,7 +33,20 @@ fn type_cmd(args: &[String]) {
     }
 }
 
+fn search(program: &String, path: &Vec<String>) -> (bool, String) {
+    for p in path {
+        for entry in WalkDir::new(p).into_iter().filter_map(|e| e.ok()) {
+            let bin = entry.path().display().to_string();
+            if bin.ends_with(&format!("/{}", program)) {
+                return (true, bin);
+             }                 
+        }
+    }
+    return (false, String::new())
+}
+
 fn main() {
+    let path : Vec<String> = env::var("PATH").unwrap().split(":").map(str::to_string).collect();
     loop {
         print!("$ ");
         io::stdout().flush().unwrap();
@@ -53,7 +68,14 @@ fn main() {
             },
             Some("echo") => echo_cmd(&args),
             Some("type") => type_cmd(&args),
-            Some(_) => println!("{}: command not found", input.trim()),
+            Some(cmd) => {
+                let (found, p) = search(&cmd.to_string(), &path);
+                if found {
+                    println!("{} is {}", cmd, p);
+                } else {
+                    println!("{}: command not found", input.trim());
+                }
+            },
             None => continue,
         }
     }
